d6803614f591f493009b4c4b6437cc01663a6873
jvitoralmeida01 <jvitoralmeida2001@gmail.com>
2022-05-08 18:04:31 -0300

    finalizing bloom effect

--- src/local_modules/SolarSystem.js
+++ src/local_modules/SolarSystem.js
@@ -1,65 +1,142 @@
 import * as THREE from 'three'
 
 export default class SolarSystem {
-    constructor(origin) {
+    constructor(origin, textureLoader) {
 
-        this.sun = this.sunCreator(origin.x, origin.y, origin.z)
+        this.sunTexture = textureLoader.load('./textures/rocky/sun.jpg');
+        this.displacementSunTexture = textureLoader.load('./textures/rocky/displacementSun.jpg');
+        
+
+        this.sun = this.sunCreator(origin.x, origin.y, origin.z, (Math.random() > 0.5));
+        this.planets = new Array(8).fill(null).map((_, index) => this.planetCreator(
+            this.sun.mesh.position,
+            0.2 + Math.random()*0.5,
+            Math.floor(Math.random()*10000000).toString(16),
+            index,
+            textureLoader
+        ));
     }
-    static sunCreator = () => {
+
+    sunCreator(x, y, z, blue = false) {
         const sunMesh = new THREE.Mesh(
-            new THREE.SphereGeometry(0.1, 32, 32),
-            new THREE.MeshBasicMaterial({color: 0xffff77})
+            new THREE.SphereGeometry(1, 32, 32),
+            new THREE.MeshBasicMaterial({
+                color: blue ? 0x55ddff : 0xffdd55,
+            })
         )
-        sunMesh.position.set(0, 0, 0)
-        return sunMesh
+        sunMesh.position.set(x, y, z);
+        const sunLight = new THREE.PointLight(0xffffaa, 1)
+        sunLight.position.set(sunMesh.position.x, sunMesh.position.y, sunMesh.position.z)
+        return {
+            mesh: sunMesh,
+            light: sunLight,
+        }
     }
 
-    static planet = (radius, color) => {
+    planetCreator(orbitCenter, radius, color, index, textureLoader) {
+        const planetTexture = textureLoader.load('./textures/rocky/' + Math.floor((Math.random()*6)) + '.jpg');
+        // when using NearestFilter as the minFilter we dont need to generate the mipmaps (better for the GPU)
+        planetTexture.generateMipmaps = false
+        planetTexture.minFilter = THREE.NearestFilter
+        planetTexture.magFilter = THREE.NearestFilter
+
+        const planetMesh = new THREE.Mesh(
+            new THREE.SphereGeometry(radius, 32, 32),
+            new THREE.MeshStandardMaterial({
+                color: 0xaaaaaa,
+                map: planetTexture,
+            })
+        )
+        planetMesh.position.set( orbitCenter.x + this.getOrbit(index), orbitCenter.y, orbitCenter.z );
+        planetMesh.layers.set(1)
+        return planetMesh;
+    }
+
+    getOrbit(index) {
+        return 5 * index;
+    }
+
+    update(time, factor) {
+        this.planets.forEach(planet => {
+            let index = this.planets.indexOf(planet)
+            let orbit = this.getOrbit(index) * (factor + 1)
+            let speed = 10 / (this.getOrbit(index))
+            planet.position.x = (Math.sin(time*speed) * orbit)
+            planet.position.z = (Math.cos(time*speed) * orbit)
+            planet.rotation.y = 2*time*speed
+        })
     }
 }
 
-// let planets = new Array()
-// let planetsOrbit = new Array()
-// const mercury = new THREE.Mesh(
-//     new THREE.SphereGeometry(0.03, 32, 32),
-//     new THREE.MeshStandardMaterial({
-//         color: 0xaaaaaa,
-//     })
-// )
-// mercury.position.x = -0.3
-// mercury.position.y = -0.6
-// planets[0] = mercury
-// planetsOrbit[0] = 0.3
-
-// const venus = new THREE.Mesh(
-//     new THREE.SphereGeometry(0.05, 32, 32),
-//     new THREE.MeshStandardMaterial({
-//         color: 0xeeff00,
-//     })
-// )
-// venus.position.x = -0.6
-// venus.position.y = -0.6
-// planets[1] = venus
-// planetsOrbit[1] = 0.6
-
-// const earth = new THREE.Mesh(
-//     new THREE.SphereGeometry(0.07, 32, 32),
-//     new THREE.MeshStandardMaterial({
-//         color: 0x0000ff,
-//     })
-// )
-// earth.position.x = -0.9
-// earth.position.y = -0.6
-// planets[2] = earth
-// planetsOrbit[2] = 0.9
-
-// const mars = new THREE.Mesh(
-//     new THREE.SphereGeometry(0.05, 32, 32),
-//     new THREE.MeshStandardMaterial({
-//         color: 0xff0000,
-//     })
-// )
-// mars.position.x = -1.2
-// mars.position.y = -0.6
-// planets[4] = mars
-// planetsOrbit[4] = 1.2
+// /**
+//  * Objects
+//  */
+//  let planets = new Array()
+//  let planetsOrbit = new Array()
+//  // Material
+//  const planeMaterial = new THREE.MeshStandardMaterial({
+//      color: 0x000000
+//  })
+//  planeMaterial.roughness = 0.4
+ 
+//  // const planetMaterial = new THREE.MeshStandardMaterial()
+//  // planetMaterial.roughness = 0.2
+ 
+//  // Planets
+//      const mercury = new THREE.Mesh(
+//          new THREE.SphereGeometry(0.03, 32, 32),
+//          new THREE.MeshStandardMaterial({
+//              color: 0xaaaaaa,
+//          })
+//      )
+//      mercury.position.x = -0.3
+//      mercury.position.y = -0.6
+//      planets[0] = mercury
+//      planetsOrbit[0] = 0.3
+ 
+//      const venus = new THREE.Mesh(
+//          new THREE.SphereGeometry(0.05, 32, 32),
+//          new THREE.MeshStandardMaterial({
+//              color: 0xeeff00,
+//          })
+//      )
+//      venus.position.x = -0.6
+//      venus.position.y = -0.6
+//      planets[1] = venus
+//      planetsOrbit[1] = 0.6
+ 
+//      const earth = new THREE.Mesh(
+//          new THREE.SphereGeometry(0.07, 32, 32),
+//          new THREE.MeshStandardMaterial({
+//              color: 0x0000ff,
+//          })
+//      )
+//      earth.position.x = -0.9
+//      earth.position.y = -0.6
+//      planets[2] = earth
+//      planetsOrbit[2] = 0.9
+ 
+//      const mars = new THREE.Mesh(
+//          new THREE.SphereGeometry(0.05, 32, 32),
+//          new THREE.MeshStandardMaterial({
+//              color: 0xff0000,
+//          })
+//      )
+//      mars.position.x = -1.2
+//      mars.position.y = -0.6
+//      planets[4] = mars
+//      planetsOrbit[4] = 1.2
+     
+//  const sunMesh = new THREE.Mesh(
+//      new THREE.SphereGeometry(0.1, 32, 32),
+//      new THREE.MeshBasicMaterial({color: 0xffff77})
+//  )
+//  sunMesh.position.set(0, 0, 0)
+ 
+//  const plane = new THREE.Mesh(
+//      new THREE.PlaneGeometry(10, 5),
+//      planeMaterial
+//  )
+//  plane.rotation.x = - Math.PI * 0.5
+//  plane.position.y = - 1
+ 
--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass.js'
+import {UnrealBloomPass} from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
+
+import SolarSystem from './local_modules/SolarSystem';
+import Stars from './local_modules/Stars';
 
 /*
  * Audio
@@ -23,17 +29,7 @@ track.connect(analyser);
  * Texture
  */
 const loadingManager = new THREE.LoadingManager();
-// loadingManager.onStart = () => {console.log("Starting")}
-// loadingManager.onLoad = () => {console.log("Loaded")}
-// loadingManager.onProgress = () => {console.log("Progress")}
-// loadingManager.onError = () => {console.log("Error")}
 const textureLoader = new THREE.TextureLoader(loadingManager);
-const colorTexture = textureLoader.load('./textures/minecraft.png');
-
-// when using NearestFilter as the minFilter we dont need to generate the mipmaps (better for the GPU)
-colorTexture.generateMipmaps = false
-colorTexture.minFilter = THREE.NearestFilter
-colorTexture.magFilter = THREE.NearestFilter
 
 /**
  * Base
@@ -44,97 +40,22 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Objects
- */
-let planets = new Array()
-let planetsOrbit = new Array()
-// Material
-const planeMaterial = new THREE.MeshStandardMaterial({
-    color: 0x000000
-})
-planeMaterial.roughness = 0.4
+// Stars
+const stars_firstLayer = new Stars(80, textureLoader);
+const stars0 = new Stars(100, textureLoader);
+const stars_lastLayer = new Stars(120, textureLoader);
 
-// const planetMaterial = new THREE.MeshStandardMaterial()
-// planetMaterial.roughness = 0.2
+// Solar System
+const solarSystem = new SolarSystem(new THREE.Vector3(0, 0, 0), textureLoader)
+solarSystem.sun.light.layers.set(1)
 
-// Planets
-    const mercury = new THREE.Mesh(
-        new THREE.SphereGeometry(0.03, 32, 32),
-        new THREE.MeshStandardMaterial({
-            color: 0xaaaaaa,
-        })
-    )
-    mercury.position.x = -0.3
-    mercury.position.y = -0.6
-    planets[0] = mercury
-    planetsOrbit[0] = 0.3
-
-    const venus = new THREE.Mesh(
-        new THREE.SphereGeometry(0.05, 32, 32),
-        new THREE.MeshStandardMaterial({
-            color: 0xeeff00,
-        })
-    )
-    venus.position.x = -0.6
-    venus.position.y = -0.6
-    planets[1] = venus
-    planetsOrbit[1] = 0.6
-
-    const earth = new THREE.Mesh(
-        new THREE.SphereGeometry(0.07, 32, 32),
-        new THREE.MeshStandardMaterial({
-            color: 0x0000ff,
-        })
-    )
-    earth.position.x = -0.9
-    earth.position.y = -0.6
-    planets[2] = earth
-    planetsOrbit[2] = 0.9
-
-    const mars = new THREE.Mesh(
-        new THREE.SphereGeometry(0.05, 32, 32),
-        new THREE.MeshStandardMaterial({
-            color: 0xff0000,
-        })
-    )
-    mars.position.x = -1.2
-    mars.position.y = -0.6
-    planets[4] = mars
-    planetsOrbit[4] = 1.2
-    
-const sunMesh = new THREE.Mesh(
-    new THREE.SphereGeometry(0.1, 32, 32),
-    new THREE.MeshBasicMaterial({color: 0xffff77})
-)
-sunMesh.position.set(0, 0, 0)
-
-const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 5),
-    planeMaterial
-)
-plane.rotation.x = - Math.PI * 0.5
-plane.position.y = - 1
-
-scene.add(...planets, plane, sunMesh)
+scene.add(...solarSystem.planets, solarSystem.sun.mesh, solarSystem.sun.light, stars_firstLayer.particles, stars0.particles, stars_lastLayer.particles);
 
 /**
  * Lights
  */
-// const rectAreaLight = new THREE.RectAreaLight(0xffffff, 1, 2, 2)
-// rectAreaLight.position.set(0, 0, -2)
-// rectAreaLight.rotation.y = Math.PI
-// scene.add(rectAreaLight)
-
-const dirLight = new THREE.DirectionalLight(0xffffff, 0.08)
-dirLight.position.set(0, 1, 1)
-scene.add(dirLight)
-
-const sunLight = new THREE.PointLight(0xffffaa, 1)
-sunLight.position.set(sunMesh.position.x, sunMesh.position.y, sunMesh.position.z)
-scene.add(sunLight)
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.1)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6)
+ambientLight.layers.set(1)
 scene.add(ambientLight)
 
 /**
@@ -165,10 +86,12 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 1
+camera.position.x = 10
+camera.position.y = 10
+camera.position.z = 10
 camera.lookAt(new THREE.Vector3(0, 0, 0))
+camera.far = stars_lastLayer.radius * 30
+camera.updateProjectionMatrix()
 scene.add(camera)
 
 // Controls
@@ -179,18 +102,52 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.autoClear = false;
+renderer.setClearColor(0xffffffff, 0.0);
+
+// Post-processing
+//bloom renderer
+const renderScene = new RenderPass(scene, camera);
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  1.5,
+  0.4,
+  0.85
+);
+bloomPass.threshold = 0.2;
+bloomPass.strength = 2; //intensity of glow
+bloomPass.radius = 0;
+const bloomComposer = new EffectComposer(renderer);
+bloomComposer.setSize(window.innerWidth, window.innerHeight);
+bloomComposer.renderToScreen = true;
+bloomComposer.addPass(renderScene);
+bloomComposer.addPass(bloomPass);
+
+solarSystem.sun.mesh.layers.set(1);
+stars_firstLayer.particles.layers.set(1);
+stars0.particles.layers.set(1);
+stars_lastLayer.particles.layers.set(1);
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
 
+// // renderer.render(scene, camera)
+// camera.layers.set(1)
+// bloomComposer.render()
 
 window.addEventListener('click', () => {
+
+    // if(!audioElement.paused) {
+    //     audioElement.pause()
+    // }
+
     audioElement.play()
     
     analyser.fftSize = 64
@@ -200,35 +157,32 @@ window.addEventListener('click', () => {
     const tick = () =>
         {
             analyser.getByteFrequencyData(dataArray);
-            for(let i = 0; i < bufferLength; i++) {
 
-            }
-            sunMesh.scale.x = 1 + dataArray[16] / 255
-            sunMesh.scale.y = 1 + dataArray[16] / 255
-            sunMesh.scale.z = 1 + dataArray[16] / 255
+            //update sun
+            solarSystem.sun.mesh.scale.x = 1 + dataArray[16] / 255
+            solarSystem.sun.mesh.scale.y = 1 + dataArray[16] / 255
+            solarSystem.sun.mesh.scale.z = 1 + dataArray[16] / 255
+
+            solarSystem.sun.light.intensity = 0.1 + dataArray[16] / 125
 
             const factor = dataArray[16] / 512
 
             const elapsedTime = clock.getElapsedTime()
 
             // Update objects
-                // update Sun
-                sunMesh.position.y = (Math.sin(elapsedTime) * 0.1) - 0.6
-                sunLight.position.y = (Math.sin(elapsedTime) * 0.1) - 0.6
-                // update planets
-                planets.forEach(planet => {
-                    let index = planets.indexOf(planet)
-                    let orbit = planetsOrbit[index]
-                    let speed = 1 / (planetsOrbit[index])
-                    planet.position.x = (Math.sin(elapsedTime*speed) * orbit)
-                    planet.position.z = (Math.cos(elapsedTime*speed) * orbit)
-                })
+            solarSystem.update(elapsedTime, factor)
+            stars_firstLayer.update(elapsedTime, 1)
+            stars0.update(elapsedTime, 0.9)
+            stars_lastLayer.update(elapsedTime, 0.8)
 
             // Update controls
             controls.update()
 
             // Render
-            renderer.render(scene, camera)
+            // camera.layers.set(0);
+            // renderer.render(scene, camera);
+            camera.layers.set(1);
+            bloomComposer.render();
 
             // Call tick again on the next frame
             window.requestAnimationFrame(tick)
